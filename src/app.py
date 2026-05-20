72bc59c358798ca9951eadd733dbede05618ec6e
Sparklyturtle17 <rafiki.erickson@gmail.com>
2026-05-20 22:34:23 +0000

    read json

--- src/app.py
+++ src/app.py
@@ -1,13 +1,17 @@
-import flask
-from flask import request, jsonify
+import os
+from flask import Flask, request, jsonify, json
 
 app = Flask(__name__)
+
+
 
 # List
 @app.route('/comments', methods=['GET'])
 def api_all():
-    result = do_query('SELECT * FROM comments;')
-    return jsonify(result)
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static/data", "comments.json")
+    data = json.load(open(json_url))
+    return jsonify(data)
 
 # Create
 @app.route('/add', methods=['POST'])
