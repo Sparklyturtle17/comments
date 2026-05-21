8cead77a3b51fbd10e4a83c62abcdd66c72e04de
Sparklyturtle17 <rafiki.erickson@gmail.com>
2026-05-21 17:55:23 +0000

    run command and add writes to file

--- src/app.py
+++ src/app.py
@@ -16,10 +16,19 @@ def api_all():
 # Create
 @app.route('/add', methods=['POST'])
 def create_record():
-    pass
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static/data", "comments.json")
+    with open(json_url, 'r') as f:
+        data = json.load(f)
+    new_comment = request.get_json()
+    data.append(new_comment)
+    with open(json_url, 'w') as f:
+        json.dump(data, f)
+    return jsonify(new_comment), 201
 
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
-app.run()
+if __name__ == "__main__":
+    app.run()
